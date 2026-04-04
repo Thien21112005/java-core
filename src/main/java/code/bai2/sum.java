e2ab5a14273b881a7e2ad724855bcb01072e8aea
Thien211105 <huyalex009@gmail.com>
2026-04-05 00:19:17 +0700

    add class sum for declare datatype and variable

---
+++ src/main/java/code/bai2/sum.java
@@ -0,0 +1,14 @@
+package code.bai2;
+
+public class sum {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int c = a + b;
+        System.out.println("Sum: " + c);
+
+        int test = 5 + 3 * 9 + 10 / 2;
+        System.out.println("Check test: " + test);
+
+    }
+}
